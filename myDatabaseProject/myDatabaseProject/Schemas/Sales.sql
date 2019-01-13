6e2033a995ff72a7aa9108dd9c0d0c4fd6ca25c4
Godfrey Dsouza <godfreyadsouza@outlook.com>
2019-01-13 15:12:04 +0000

    add database objects

---
+++ myDatabaseProject/myDatabaseProject/Schemas/Sales.sql
@@ -0,0 +1,5 @@
+﻿CREATE SCHEMA [Sales]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Contains objects related to customers, sales orders, and sales territories.' , @level0type=N'SCHEMA',@level0name=N'Sales'
+GO
---
+++ myDatabaseProject/myDatabaseProject/StoredProcedures/uspGetEmployeeDepartmentManagers.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[uspGetEmployeeDepartmentManagers]
+AS
+BEGIN
+    SET NOCOUNT ON;
+	;WITH Employee_Department (DepartmentID, FirstName, LastName, DepartmentName)
+	AS 
+	(
+		SELECT D.DepartmentID, E.FirstName, E.LastName, D.[Name] FROM Department D 
+		LEFT JOIN Employee E ON D.DepartmentID = E.DepartmentID
+	),
+	Department_Manager (DepartmentID,Department, DepartmentManager)
+	AS
+	(
+		SELECT D.DepartmentID, D.Name, CONCAT(E.FirstName, ' ', E.LastName) FROM
+		Department D LEFT JOIN Employee E ON D.DepartmentManager = E.EmployeeID
+	)
+	SELECT ED.FirstName, ED.LastName, ED.DepartmentName, DM.DepartmentManager FROM 
+	Employee_Department ED
+	LEFT JOIN Department_Manager DM ON ED.DepartmentID = DM.DepartmentID
+END;
+GO
